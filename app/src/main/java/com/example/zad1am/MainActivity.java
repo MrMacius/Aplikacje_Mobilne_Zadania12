9703fb36c85dcc6d0d56180afdf066a1b572c58c
MrMacius <lukasz.leonczak69@gmail.com>
2023-10-24 06:54:37 +0200

    zadanie 2 ale nie skonczone

--- app/src/main/java/com/example/zad1am/MainActivity.java
+++ app/src/main/java/com/example/zad1am/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.zad1am;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,16 +12,23 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String KEY_CURRENT_INDEX = "currentIndex";
     private Button trueButton;
     private Button falseButton;
     private Button nextButton;
     private TextView questionTextView;
     private TextView counterCorrect;
 
+    private static final String TAG = "ZAD1AM";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: ");
         setContentView(R.layout.activity_main);
+        if(savedInstanceState != null){
+            currentIndex = savedInstanceState.getInt(KEY_CURRENT_INDEX);
+        }
 
         trueButton = findViewById(R.id.true_button);
         falseButton = findViewById(R.id.false_button);
@@ -52,6 +61,43 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: ");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "WYWOŁANA ZOSTAŁA METODA onSaveInstanceState");
+        outState.putInt(KEY_CURRENT_INDEX,currentIndex);
+    }
+
     private Question[] questions = new Question[]{
             new Question(R.string.q_cpp,false),
             new Question(R.string.q_java,true),
@@ -70,7 +116,7 @@ public class MainActivity extends AppCompatActivity {
         int resultMessageId = 0;
         if(answer == correctAnswer){
             resultMessageId = R.string.correct_answer;
-            correctCounter = correctCounter+1;
+            if(correctCounter <6){correctCounter = correctCounter+1;}
             counterCorrect.setText(String.valueOf(correctCounter)+"/6 poprawnych odpowiedzi");
         }else{
             resultMessageId = R.string.incorrect_answer;
